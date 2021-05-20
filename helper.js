8ef87433e6d8e504386ee5b831f267b15ac7bd75
dkhole <dkhlee419@gmail.com>
2021-05-20 14:51:20 +1000

    updated restrictions

--- helper.js
+++ helper.js
@@ -1,45 +1,42 @@
 const fs = require('fs');
 
 module.exports = {
-    readFiles: () => {
-        const users = fs.readFileSync('./users.txt', 'utf8').split('\n');
-        const listings = fs.readFileSync('./listings.txt', 'utf8').split('\n');
-        const messages = fs.readFileSync('./messages.txt', 'utf8').split('\n');
-    
-        const userRows = users.map((row) => {
-            const userInfo = row.split(' ');
-            return { username: userInfo[0], password: userInfo[1]};
-        });
+	readFiles: () => {
+		const users = fs.readFileSync('./users.txt', 'utf8').split('\n');
+		const listings = fs.readFileSync('./listings.txt', 'utf8').split('\n');
+		const messages = fs.readFileSync('./messages.txt', 'utf8').split('~\n');
 
-        return [userRows, listings, messages];
-    },
+		const userRows = users.map((row) => {
+			const userInfo = row.split(' ');
+			return { username: userInfo[0], password: userInfo[1] };
+		});
 
-    login: async(page, email, password) => {
-        await page.goto('https://www.gumtree.com.au/t-login-form.html', {waitUntil: 'domcontentloaded'});
-        //https://www.gumtree.com.au/t-login-form.html?sl=true
-        console.log('🚀   Entering email   🚀');
-        await page.waitForSelector('#login-email');
-        await page.type('#login-email', email);
-      
-        console.log('🚀   Entering password   🚀');
-        await page.type('#login-password', password);
-      
-        console.log('🚀   Logging in   🚀');
-      
-        await Promise.all([
-            page.click('#btn-submit-login'),
-            page.waitForNavigation(),
-        ]);
-        console.log(`🚀   Logged In to gumtree as ${email}   🚀`);
-    },
+		return [userRows, listings, messages];
+	},
 
-    sendMessage: async(page, listing, message) => {
-        console.log(`🚀   Navigating to listing   🚀`);
-        await page.goto(listing, {waitUntil: 'domcontentloaded'});
-        const text = await page.waitForSelector('#input-reply-widget-form-message');
-        await text.click({ clickCount: 3 })
-        await page.type('#input-reply-widget-form-message', message);
-        /*await Promise.all([
+	login: async (page, email, password) => {
+		await page.goto('https://www.gumtree.com.au/t-login-form.html', { waitUntil: 'domcontentloaded' });
+		//https://www.gumtree.com.au/t-login-form.html?sl=true
+		console.log('🚀   Entering email   🚀');
+		await page.waitForSelector('#login-email');
+		await page.type('#login-email', email);
+
+		console.log('🚀   Entering password   🚀');
+		await page.type('#login-password', password);
+
+		console.log('🚀   Logging in   🚀');
+
+		await Promise.all([page.click('#btn-submit-login'), page.waitForNavigation()]);
+		console.log(`🚀   Logged In to gumtree as ${email}   🚀`);
+	},
+
+	sendMessage: async (page, listing, message) => {
+		console.log(`🚀   Navigating to listing   🚀`);
+		await page.goto(listing, { waitUntil: 'domcontentloaded' });
+		const text = await page.waitForSelector('#input-reply-widget-form-message');
+		await text.click({ clickCount: 3 });
+		await page.type('#input-reply-widget-form-message', message);
+		/*await Promise.all([
             page.click('#contact-seller-button'),
             page.waitForNavigation(),
             page.click('.icon-close'),
@@ -47,5 +44,19 @@ module.exports = {
             page.click('#MY_GUMTREE_MENU_BUTTON'),
         ]);
         console.log(`🚀       Sent message        🚀`);*/
-    }
-}
+	},
+
+	sleep: async (ms) => {
+		console.log(`sleeping for ${ms} ms`);
+		return new Promise((resolve) => {
+			setTimeout(resolve, ms);
+		});
+	},
+
+	resetIndex: (index, length) => {
+		if (index >= length) {
+			index = 0;
+		}
+		return index;
+	},
+};
--- index.js
+++ index.js
@@ -1,55 +1,67 @@
 const puppeteer = require('puppeteer');
 const helper = require('./helper');
 
-const resetIndex = (index, length) => {
-    if(index >= length ) {
-        index = 0;
-    }
-    return index;
-}
-
 const launch = async () => {
-    const browser = await puppeteer.launch({ headless: false });
-    const [page] = await browser.pages();
-    
-    //tricks gumtree
-    await page.evaluateOnNewDocument(() => {
-        Object.defineProperty(navigator, 'webdriver', {
-            get: () => false
-        });
-    });
+	//get data from txt files
+	const [userRows, listings, messages] = helper.readFiles();
 
-    //get data from txt files
-    const [userRows, listings, messages] = helper.readFiles();
+	//handle edge cases
+	if (userRows.length < 1) {
+		console.log('minimum 1 user. Please update users.txt.');
+		return;
+	}
+	if (messages.length < 2) {
+		console.log('minimum 2 messages. Please update messages.txt.');
+		return;
+	}
+	if (listings.length < 1) {
+		console.log('minimum of 2 listings. Please update listings.txt.');
+		return;
+	}
 
+	const browser = await puppeteer.launch({ headless: false });
+	const [page] = await browser.pages();
 
-    //await helper.login(page, 'notabot419@outlook.com', 'notabot419');    
-    //await helper.sendMessage(page, 'https://www.gumtree.com.au/s-ad/randwick/beds/target-double-bed-with-firm-foam-mattress/1274373005', 'Please let me know as i can pick it up tomorrow');
-    //await browser.close();
+	//tricks gumtree
+	await page.evaluateOnNewDocument(() => {
+		Object.defineProperty(navigator, 'webdriver', {
+			get: () => false,
+		});
+	});
 
+	//await helper.login(page, 'notabot419@outlook.com', 'notabot419');
+	//await helper.sendMessage(page, 'https://www.gumtree.com.au/s-ad/randwick/beds/target-double-bed-with-firm-foam-mattress/1274373005', 'Please let me know as i can pick it up tomorrow');
 
-//loop through users, loop through messages, send to all listings, wait 3 minutes between listings.
-    let usersIndex = 0;
-    let messagesIndex = 0;
-    const usersLength = userRows.length
-    const messagesLength = messages.length
+	//loop through users, loop through messages, send to all listings, wait 3 minutes between listings.
+	let usersIndex = 0;
+	let messagesIndex = 0;
+	const usersLength = userRows.length;
+	const messagesLength = messages.length;
 
-    for(listing of listings) {
-        //login with user
-        usersIndex = resetIndex(usersIndex, usersLength);
-        messagesIndex = resetIndex(messagesIndex, messagesLength);
+	console.log(messages);
 
-        await helper.login(page, userRows[usersIndex].username, userRows[usersIndex].password);    
-        //console.log('logging in with ' + userRows[usersIndex].username + ' and ' + userRows[usersIndex].password);
-        await helper.sendMessage(page, listing, messages[messagesIndex]);
-        //console.log('sending message ' + messages[messagesIndex] + ' to listing ' + listing);
-        //console.log(messagesIndex);
-        usersIndex++;
-        messagesIndex++;
-    }
-    //console.log(userRows);
-    //console.log(listings);
-    //console.log(count);
-}
+	for (let listingsInd = 0; listingsInd < listings.length; listingsInd++) {
+		//login with user
+		usersIndex = helper.resetIndex(usersIndex, usersLength);
+		messagesIndex = helper.resetIndex(messagesIndex, messagesLength);
+
+		await helper.login(page, userRows[usersIndex].username, userRows[usersIndex].password);
+		//console.log('logging in with ' + userRows[usersIndex].username + ' and ' + userRows[usersIndex].password);
+		await helper.sendMessage(page, listings[listingsInd], messages[messagesIndex]);
+		messagesIndex++;
+		listingsInd++;
+		//wait a minute
+		await helper.sleep(3000);
+		await helper.sendMessage(page, listings[listingsInd], messages[messagesIndex]);
+		messagesIndex++;
+		usersIndex++;
+		//console.log('sending message ' + messages[messagesIndex] + ' to listing ' + listing);
+		//console.log(messagesIndex);
+	}
+	//console.log(userRows);
+	//console.log(listings);
+	//console.log(count);
+	await browser.close();
+};
 
 launch();
