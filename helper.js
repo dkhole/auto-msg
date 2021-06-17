ec26f01225d5fd726d2c56a47ba592b465918247
dkhole <dkhlee419@gmail.com>
2021-06-17 17:17:45 +1000

    changed it so we scrape and map category for user

--- helper.js
+++ helper.js
@@ -1,6 +1,141 @@
 const fs = require('fs');
+const puppeteer = require('puppeteer');
+const readline = require('readline');
+const cheerio = require('cheerio');
+const MapCategories = require('./mapCategories');
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
+
+const login = async (page, email, password) => {
+	await page.goto('https://www.gumtree.com.au/t-login-form.html', { waitUntil: 'domcontentloaded' });
+	console.log('🚀   Entering email   🚀');
+	await page.waitForSelector('#login-email');
+	await page.type('#login-email', email);
+
+	console.log('🚀   Entering password   🚀');
+	await page.type('#login-password', password);
+
+	console.log('🚀   Logging in   🚀');
+
+	await Promise.all([page.click('#btn-submit-login'), page.waitForNavigation()]);
+	console.log(`🚀   Logged In to gumtree as ${email}   🚀`);
+};
+
+const sendMessage = async (page, listing, message) => {
+	console.log(`🚀   Navigating to listing   🚀`);
+	await page.goto(listing.url, { waitUntil: 'domcontentloaded' });
+	await page.setDefaultNavigationTimeout(300000);
+	const text = await page.waitForSelector('#input-reply-widget-form-message');
+	await text.click({ clickCount: 3 });
+
+	//get and map category
+	console.log(`🚀   Getting and mapping category   🚀`);
+	await page.waitForSelector('.breadcrumbs__separator');
+	const content = await page.content();
+	const $ = cheerio.load(content);
+	const breadcrumbs = $('.breadcrumbs__separator');
+	const category = breadcrumbs.last().prev();
+	const mappedCat = MapCategories.mapCategories(category.text());
+	if(!mappedCat) {
+		console.log('Error with mapping category');
+		return;
+	}
+	//add mapped category into message
+	console.log(`🚀   Typing and sending   🚀`);
+	const send = message.replace('$', mappedCat);
+	await page.type('#input-reply-widget-form-message', send);
+	await Promise.all([page.click('#contact-seller-button'), page.waitForNavigation()]);
+	console.log(`🚀       Sent message        🚀`);
+};
+
+const sleep = async (ms) => {
+	console.log(`🚀     sleeping for ${ms} ms     🚀`);
+	return new Promise((resolve) => {
+		setTimeout(resolve, ms);
+	});
+};
+
+const resetIndex = (index, length) => {
+	if (index >= length) {
+		index = 0;
+	}
+	return index;
+};
+
+const recursiveAsyncReadLineBrowser = (userRows, listings, messages) => {
+	rl.question('Are you ready to start? (yes/no)', (browser) => {
+		if (browser === 'yes') {
+			startMessaging(userRows, listings, messages);
+			rl.close();
+		} else if (browser === 'no') {
+			console.log('Ctrl + C to quit / restart');
+			recursiveAsyncReadLineBrowser(userRows, listings, messages);
+		} else {
+			recursiveAsyncReadLineBrowser(userRows, listings, messages);
+		}
+	});
+};
+
+const startMessaging = async (userRows, listings, messages) => {
+	//launch puppeteer
+	const browser = await puppeteer.launch({ headless: false });
+	const [page] = await browser.pages();
+
+	//gets past gumtrees antiscrape
+	await page.evaluateOnNewDocument(() => {
+		Object.defineProperty(navigator, 'webdriver', {
+			get: () => false,
+		});
+	});
+
+	//loop through users, loop through messages, send to all listings, wait 3 minutes between listings.
+
+	let usersIndex = 0;
+	let messagesIndex = 0;
+	const usersLength = userRows.length;
+	const messagesLength = messages.length;
+	//loop through listings
+	for (let listingsInd = 0; listingsInd < listings.length; listingsInd++) {
+		//reset index for users/messages to repeat them if we've reached the end
+		usersIndex = resetIndex(usersIndex, usersLength);
+		messagesIndex = resetIndex(messagesIndex, messagesLength);
+
+		await login(page, userRows[usersIndex].username, userRows[usersIndex].password);
+		console.log('logging in with ' + userRows[usersIndex].username + ' and ' + userRows[usersIndex].password);
+		await sendMessage(page, listings[listingsInd], messages[messagesIndex]);
+		messagesIndex++;
+		listingsInd++;
+		//wait two minutes
+		await sleep(20000);
+		await sendMessage(page, listings[listingsInd], messages[messagesIndex]);
+		messagesIndex++;
+		usersIndex++;
+	}
+	await browser.close();
+};
 
 module.exports = {
+	recursiveAsyncReadLineData: (userRows, listings, messages) => {
+		rl.question('Do you want to view the input data? (yes/no)', (view) => {
+			if (view === 'yes') {
+				console.log(`~ Users (${userRows.length})`);
+				console.log(userRows);
+				console.log(`~ Listings (${listings.length})`);
+				console.log(listings);
+				console.log(`~ Messages (${messages.length})`);
+				console.table(messages);
+				recursiveAsyncReadLineBrowser(userRows, listings, messages);
+			} else if (view === 'no') {
+				recursiveAsyncReadLineBrowser(userRows, listings, messages);
+			} else {
+				recursiveAsyncReadLineData(userRows, listings, messages);
+			}
+		});
+	},
+
 	readFiles: () => {
 		const users = fs.readFileSync('./src/users.txt', 'utf8').split('\n');
 		const listings = fs.readFileSync('./src/listings.txt', 'utf8').split('\n');
@@ -12,51 +147,9 @@ module.exports = {
 		});
 
 		const listingsRow = listings.map((listing) => {
-			const listingInfo = listing.split(' ');
-			return { url: listingInfo[1], category: listingInfo[0] };
+			return { url: listing };
 		});
 
 		return [userRows, listingsRow, messages];
-	},
-
-	login: async (page, email, password) => {
-		await page.goto('https://www.gumtree.com.au/t-login-form.html', { waitUntil: 'domcontentloaded' });
-		console.log('🚀   Entering email   🚀');
-		await page.waitForSelector('#login-email');
-		await page.type('#login-email', email);
-
-		console.log('🚀   Entering password   🚀');
-		await page.type('#login-password', password);
-
-		console.log('🚀   Logging in   🚀');
-
-		await Promise.all([page.click('#btn-submit-login'), page.waitForNavigation()]);
-		console.log(`🚀   Logged In to gumtree as ${email}   🚀`);
-	},
-
-	sendMessage: async (page, listing, message) => {
-		console.log(`🚀   Navigating to listing   🚀`);
-		await page.goto(listing.url, { waitUntil: 'domcontentloaded' });
-		await page.setDefaultNavigationTimeout(300000);
-		const text = await page.waitForSelector('#input-reply-widget-form-message');
-		await text.click({ clickCount: 3 });
-		const send = message.replace('$', listing.category);
-		await page.type('#input-reply-widget-form-message', send);
-		//await Promise.all([page.click('#contact-seller-button'), page.waitForNavigation()]);
-		//console.log(`🚀       Sent message        🚀`);
-	},
-
-	sleep: async (ms) => {
-		console.log(`🚀     sleeping for ${ms} ms     🚀`);
-		return new Promise((resolve) => {
-			setTimeout(resolve, ms);
-		});
-	},
-
-	resetIndex: (index, length) => {
-		if (index >= length) {
-			index = 0;
-		}
-		return index;
 	},
 };
--- index.js
+++ index.js
@@ -1,80 +1,4 @@
-const puppeteer = require('puppeteer');
-const readline = require('readline');
 const helper = require('./helper');
-
-const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout,
-});
-
-const recursiveAsyncReadLineBrowser = (userRows, listings, messages) => {
-	rl.question('Are you ready to start? (yes/no)', (browser) => {
-		if (browser === 'yes') {
-			startMessaging(userRows, listings, messages, false);
-			rl.close();
-		} else if (browser === 'no') {
-			console.log('Ctrl + C to quit / restart');
-			recursiveAsyncReadLineBrowser(userRows, listings, messages);
-		} else {
-			recursiveAsyncReadLineBrowser(userRows, listings, messages);
-		}
-	});
-};
-
-const recursiveAsyncReadLineData = (userRows, listings, messages) => {
-	rl.question('Do you want to view the data? (yes/no)', (view) => {
-		if (view === 'yes') {
-			console.log(userRows);
-			console.log(listings);
-			console.table(messages);
-			recursiveAsyncReadLineBrowser(userRows, listings, messages);
-		} else if (view === 'no') {
-			recursiveAsyncReadLineBrowser(userRows, listings, messages);
-		} else {
-			recursiveAsyncReadLineData(userRows, listings, messages);
-		}
-	});
-};
-
-const startMessaging = async (userRows, listings, messages, isHeadless) => {
-	const browser = await puppeteer.launch({ headless: isHeadless });
-	const [page] = await browser.pages();
-
-	//tricks gumtree
-	await page.evaluateOnNewDocument(() => {
-		Object.defineProperty(navigator, 'webdriver', {
-			get: () => false,
-		});
-	});
-
-	//await helper.login(page, 'notabot419@outlook.com', 'notabot419');
-	//await helper.sendMessage(page, 'https://www.gumtree.com.au/s-ad/randwick/beds/target-double-bed-with-firm-foam-mattress/1274373005', 'Please let me know as i can pick it up tomorrow');
-
-	//loop through users, loop through messages, send to all listings, wait 3 minutes between listings.
-
-	let usersIndex = 0;
-	let messagesIndex = 0;
-	const usersLength = userRows.length;
-	const messagesLength = messages.length;
-
-	for (let listingsInd = 0; listingsInd < listings.length; listingsInd++) {
-		//login with user
-		usersIndex = helper.resetIndex(usersIndex, usersLength);
-		messagesIndex = helper.resetIndex(messagesIndex, messagesLength);
-
-		await helper.login(page, userRows[usersIndex].username, userRows[usersIndex].password);
-		//console.log('logging in with ' + userRows[usersIndex].username + ' and ' + userRows[usersIndex].password);
-		await helper.sendMessage(page, listings[listingsInd], messages[messagesIndex]);
-		messagesIndex++;
-		listingsInd++;
-		//wait a minute
-		await helper.sleep(10000);
-		await helper.sendMessage(page, listings[listingsInd], messages[messagesIndex]);
-		messagesIndex++;
-		usersIndex++;
-	}
-	await browser.close();
-};
 
 const launch = async () => {
 	//get data from txt files
@@ -97,7 +21,7 @@ const launch = async () => {
 	console.log(`Loaded ${messages.length} messages from messages.txt`);
 	console.log(`Loaded ${listings.length} listings from listings.txt`);
 
-	recursiveAsyncReadLineData(userRows, listings, messages);
+	helper.recursiveAsyncReadLineData(userRows, listings, messages);
 };
 
 launch();
